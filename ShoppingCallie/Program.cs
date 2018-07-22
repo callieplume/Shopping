830793b79211d9b8f5019cebe5f2fb3ff7969e56
Ernest Lukos <elukos@workforcesoftware.com>
2018-07-22 16:14:10 +0100

    Add shopping list, basket and item totals

--- ShoppingCallie/Program.cs
+++ ShoppingCallie/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ShoppingCallie
 {
@@ -6,7 +7,68 @@ namespace ShoppingCallie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int[] prices = new int[3];
+            //string[] items = { "Paint", "Paper", "Glitter" };
+            // For loop for an array
+            //for (int counter = 0; counter < items.Length; counter++)
+            //{
+            //    Console.WriteLine( counter+1 + ". " + items[counter]);
+            //}
+
+            List<string> basket = new List<string>();
+
+            IDictionary<string, double> itemLookup = new Dictionary<string, double>();
+            itemLookup["Paint"] = 3.99;
+            itemLookup["Paper"] = 1.00;
+            itemLookup["Glitter"] = 2.50;
+
+            Console.WriteLine("Welcome to Callies Crafts \n----------------- \nToday you can buy:");
+            int counter = 1;
+            foreach (KeyValuePair<string, double> item in itemLookup)
+            {
+                Console.WriteLine(counter + ". " + item.Key + " = £" + string.Format("{0:0.00}", item.Value));
+                counter++;
+            }
+
+
+            Console.WriteLine("What would you like to buy?");
+
+            string userInput = "";
+            double basketTotal = 0.00;
+            string newItem = "";
+
+            while (userInput != "1")
+            {
+                newItem = Console.ReadLine().ToLower();
+                newItem = newItem.Substring(0).ToUpper()[0] + newItem.Substring(1);
+                if (newItem == "1")
+                {
+                    break;
+                }
+
+                if (!itemLookup.ContainsKey(newItem))
+                {
+                    Console.WriteLine(newItem + " does not exist. Please try again. \nPress '1' to checkout");
+                    continue;
+                }
+                else
+                {
+                    basket.Add(newItem);
+                    basketTotal = basketTotal + itemLookup[newItem];
+
+                    Console.WriteLine("Added to basket: " + newItem + ". Press '1' to checkout");
+                    continue;     
+                }
+
+            }
+
+            Console.WriteLine("Your basket contains: ");
+            foreach (var basketItem in basket)
+            {
+                Console.WriteLine(basketItem + " = £" + string.Format("{0:0.00}", itemLookup[basketItem]));
+            }
+            Console.WriteLine("----------------- \nBasket total: £" + string.Format("{0:0.00}", basketTotal));
+
             Console.ReadLine();
         }
     }
